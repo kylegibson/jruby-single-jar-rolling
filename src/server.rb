800ab91e2ec8b2b458a77a8f18efc000ee9a389c
Kyle Gibson <kyle.gibson@atomicobject.com>
2010-08-13 11:02:50 -0400

    expanded tests for server

--- src/server.rb
+++ src/server.rb
@@ -1,5 +1,9 @@
 class Server
+  def initialize client_factory
+    @client_factory = client_factory
+  end
   def handle_accept
-    accept
+    sock, addr = accept
+    @client_factory.create(sock, addr)
   end
 end
--- tests/server_spec.rb
+++ tests/server_spec.rb
@@ -1,8 +1,35 @@
 require File.expand_path("spec_helper.rb", File.dirname(__FILE__))
 
 describe Server do
-  it '#handle_accept' do
-    subject.should_receive(:accept)
-    subject.handle_accept
+  describe '#handle_accept' do
+
+    before do
+      @client_factory = mock 'client factory'
+      @server = described_class.new @client_factory
+    end
+
+    subject { @server }
+
+    context "when initialized" do
+      it { should respond_to :handle_accept }
+    end
+
+    context "when accepting a connection" do
+      before do
+        subject.stub!(:accept).with(no_args()).and_return([:sock, :addr])
+        @client_factory.stub!(:create)
+      end
+
+      it "calls accept" do
+        subject.should_receive(:accept).with(no_args())
+        subject.handle_accept
+      end
+
+      it 'creates a client' do
+        @client_factory.should_receive(:create).with(:sock, :addr)
+        subject.handle_accept
+      end
+    end
+
   end
 end
