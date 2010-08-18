b0eab3a9b8e3d4b05e7afb058be487c226564052
Kyle Gibson <kyle.gibson@atomicobject.com>
2010-08-18 16:40:23 -0400

    continued progress

--- tests/nutwow_spec.rb
+++ tests/nutwow_spec.rb
@@ -1,15 +1,5 @@
 require File.expand_path("spec_helper.rb", File.dirname(__FILE__))
 
-describe Nutwow do
-  describe :boot do
-    context "when acting as a client" do
-      subject { described_class.new :client }
-      it { should_not be_nil }
-    end
-
-    context "when acting as a server" do
-      subject { described_class.new :server }
-      it { should_not be_nil }
-    end
-  end
+describe NutWOW do
+  it { should respond_to :run }
 end
--- tests/spec_helper.rb
+++ tests/spec_helper.rb
@@ -1,5 +1,9 @@
-def require_all_files_in_path path
-  Dir["#{path}/*.rb"].each{ |f| require f }  
-end
+include Java
 
-require_all_files_in_path File.expand_path("../src", File.dirname(__FILE__))
+path = File.expand_path("../src", File.dirname(__FILE__))
+$LOAD_PATH << path
+
+def require_all_files_in_path path
+  Dir["#{path}/*.rb"].select{|f|f!="application_bootstrap.rb"}.each{|f|require f}  
+end
+require_all_files_in_path path 
