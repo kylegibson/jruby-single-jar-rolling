dbe4cb386e5b4aafd4220bd5d9c9f9f7d15a3bb5
Kyle Gibson <kyle.gibson@atomicobject.com>
2010-08-18 14:21:15 -0400

    gui hello world

--- src/nutwow.rb
+++ src/nutwow.rb
@@ -1,8 +1,25 @@
 class NutWOW
+  import javax.swing.JFrame
+  import javax.swing.JPanel
+  import javax.swing.JLabel
+  import java.awt.Dimension
+
   def initialize 
   end
 
   def run
     puts "I am running!"
+    frame = JFrame.new
+    panel = JPanel.new
+    label = JLabel.new 'Hello World!'
+
+    panel.add label
+    frame.add panel
+
+    frame.size = Dimension.new 200, 100
+    frame.default_close_operation = JFrame::EXIT_ON_CLOSE
+    frame.visible = true
+
+    frame
   end
 end
